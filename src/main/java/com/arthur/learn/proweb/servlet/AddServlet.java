3116135f1e7e716b4cd7c98243cc94950453bb6a
Neko <neko@japan.jp>
2024-02-15 11:21:23 -0800

    Added serlvet

---
+++ src/main/java/com/arthur/learn/proweb/servlet/AddServlet.java
@@ -0,0 +1,28 @@
+package com.arthur.learn.proweb.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.arthur.learn.proweb.dao.base.BaseDao;
+import com.arthur.learn.proweb.entity.Fruit;;
+
+public class AddServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String fname = request.getParameter("fname");
+        Double price = Double.parseDouble(request.getParameter("price"));
+        Integer fcount = Integer.parseInt(request.getParameter("fcount"));
+        String remark = request.getParameter("remark");
+
+        BaseDao<Fruit> baseDao = new BaseDao<Fruit>();
+        String sql = "INSERT INTO fruits (fname, price, fcount, remark) VALUES ( ?, ?, ?, ?)";
+        int rows = baseDao.update(sql, fname, price, fcount, remark);
+        System.out.println("Number of rows affected: " + rows);
+
+    }
+}
---
+++ src/test/java/com/arthur/learn/proweb/TestFruit.java
@@ -0,0 +1,25 @@
+package com.arthur.learn.proweb;
+
+import org.junit.Test;
+
+import com.arthur.learn.proweb.dao.base.BaseDao;
+import com.arthur.learn.proweb.entity.Fruit;
+
+public class TestFruit {
+    private BaseDao<Fruit> baseDao = new BaseDao<>();
+    
+    @Test
+    public void testGetSingleBean(){
+        String sql = "Select * from fruits where fid = ?";
+        Fruit fruit = baseDao.getSingleBean(sql, Fruit.class, 1);
+        System.out.println("Fruit = " + fruit);
+    }
+
+    @Test
+    public void testInsertFruit(){
+        String sql = "INSERT INTO fruits (fname, price, fcount, remark) VALUES ( ?, ?, ?, ?)";
+        int rows = baseDao.update(sql, "Strawberry", 3, 20, "New");
+        System.out.println(rows);
+    }
+    
+}
